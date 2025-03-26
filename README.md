a716a05f592766fd9803a307a049f4fd3bb121c3
Kai Siren <coilysiren@gmail.com>
2025-03-25 18:21:35 -0700

    security

--- README.md
+++ README.md
@@ -3,5 +3,5 @@
 ## dev
 
 ```bash
-poetry run uvicorn src.main:app --reload
+poetry run uvicorn src.main:app --reload --port 3000 --host 0.0.0.0
 ```
--- src/main.py
+++ src/main.py
@@ -1,24 +1,43 @@
 import os
-import uvicorn
+
+import dotenv
 import fastapi
 import fastapi.middleware.cors as cors
+import fastapi.middleware.trustedhost as trustedhost
+import slowapi
+import slowapi.errors
+import slowapi.util
+
+dotenv.load_dotenv()
 
 app = fastapi.FastAPI()
 app.add_middleware(
     cors.CORSMiddleware,
-    allow_origins=[os.getenv("ALLOWED_ORIGINS")],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_origins=[
+        "" if os.getenv("PRODUCTION") == "True" else "localhost",
+        "https://coilysiren.me/",
+        "https://api.coilysiren.me/",
+    ],
 )
+app.add_middleware(
+    trustedhost.TrustedHostMiddleware,
+    allowed_hosts=[
+        "" if os.getenv("PRODUCTION") == "True" else "localhost",
+        "coilysiren.me",
+        "api.coilysiren.me",
+    ],
+)
+
+limiter = slowapi.Limiter(key_func=slowapi.util.get_remote_address)
+app.state.limiter = limiter
+# pylint: disable=protected-access
+# The docs for slowapi reccomend this... for whatever reason
+# docs: https://slowapi.readthedocs.io/en/latest/
+app.add_exception_handler(slowapi.errors.RateLimitExceeded, slowapi._rate_limit_exceeded_handler)  # type: ignore
+# pylint: enable=protected-access
 
 
 @app.get("/")
-def root():
-    a = "a"
-    b = "b" + a
-    return {"hello world": b}
-
-
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+@limiter.limit("1/second")
+async def root(request: fastapi.Request):
+    return {"hello world": 100}
