583ecb6fe8512fef4906ee7a74925a8bf40e0713
kai [they] <coilysiren@gmail.com>
2025-03-29 10:40:45 -0700

    debugging

--- procfile.sh
+++ procfile.sh
@@ -6,5 +6,6 @@ opentelemetry-instrument \
   --exporter_otlp_endpoint "https://api.honeycomb.io" \
   --exporter_otlp_headers "$OTEL_EXPORTER_OTLP_HEADERS" \
   --exporter_otlp_protocol "http/protobuf" \
+  --log_level "DEBUG" \
   --service_name "backend" \
   uvicorn src.main:app --host 0.0.0.0 --port "$PORT"
